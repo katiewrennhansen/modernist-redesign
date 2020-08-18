e960edd9be7df4704af179a864f6652f31558486
Katie Hansen <khansen0415@gmail.com>
2020-08-18 16:44:20 -0600

    update db

--- gis_system/index.js
+++ gis_system/index.js
@@ -31,17 +31,19 @@ function populateHouses(house){
     houseOption.classList.add('house-card')
     const houseLabel = document.createElement('h4');
     const houseDescription = createDescription(house)
+    const houseAddress = createAddress(house);
     let images;
-    if(house.attributes.image.includes(',')){
+    if(house.attributes.image && house.attributes.image.includes(',')){
         images = house.attributes.image.split(',')
-    }
-    if(images) {
+    } 
+    if(images && images.length) {
         images.map(image => createImage(image, houseOption))
-    } else {
+    } else if (images) {
         createImage(house.attributes.image, houseOption)
     }
     houseLabel.innerText = `${house.attributes.name}`;
     houseOption.appendChild(houseLabel)
+    houseOption.appendChild(houseAddress)
     houseOption.appendChild(houseDescription)
     mainContent.appendChild(houseOption)
 }
@@ -49,10 +51,11 @@ function populateHouses(house){
 function createDescription(house){
     const houseDescription = document.createElement('p');
     const houseDescription1 = document.createElement('span');
-    
-    houseDescription1.innerText = `${house.attributes.description} `;
-    houseDescription.appendChild(houseDescription1);
-
+    if(house.attributes.description){
+        houseDescription1.innerText = `${house.attributes.description} `;
+        houseDescription.appendChild(houseDescription1);
+    }
+   
     if(house.attributes.description2){
         const houseDescription2 = document.createElement('span');
         houseDescription2.innerText = `${house.attributes.description2}`;
@@ -67,4 +70,11 @@ function createImage(image, parent){
     houseImage.setAttribute('src', image)
     houseImage.classList.add('architect-image')
     parent.appendChild(houseImage)
+}
+
+function createAddress(house){
+    const houseAddress = document.createElement('address');
+    houseAddress.innerText = `${house.attributes.address}`;
+    houseAddress.classList.add('architect-address');
+    return houseAddress
 }
